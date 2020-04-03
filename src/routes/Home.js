45a80d74ac3165d4f9b80f35f1d5cce28969cf1a
Miles Higbie <miles.higbie@gmail.com>
2020-04-03 10:31:12 -0400

    set up state and fetches for sources for a user on home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
 class Home extends React.Component {
+
+    state = {
+        sources: [],
+        sourceHeadlines: []
+    }
+
+    componentDidMount(){
+        fetch(`http://localhost:3000/users/${localStorage.user_id}`)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response.sources)
+            this.setState({
+                sources: response.sources
+            })
+        }).then( () => this.state.sources.forEach(source => 
+            {
+                fetch(`https://newsapi.org/v2/top-headlines?sources=${source.name}&apiKey=03c2753b10984b3ca161dbaf9e6bf35b`)
+                .then(response => response.json())
+                .then(response => {
+                    console.log(response.articles)
+                    this.setState({
+                        sourceHeadlines: response.articles
+                    })
+                })
+        }))
+    }
+
     render(){
         return(
-            <h1> Home Home </h1>
+            <ul>
+                {this.state.sourceHeadlines.map(article => <li key={article.id}> {article.title}</li>)}
+            </ul>
         )
     }
 }
