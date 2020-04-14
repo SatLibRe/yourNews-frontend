1c1818544db1c951a9a667c60ce5070b23fed9e5
Miles Higbie <miles.higbie@gmail.com>
2020-04-14 16:32:34 -0400

    set up the dux

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux'
+// import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
 
 ReactDOM.render(<Router><Route path="/" component={App} /></Router>, 
     document.getElementById('root'));
